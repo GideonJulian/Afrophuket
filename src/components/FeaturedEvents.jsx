cf90849c68196ccfe9f1c9d83e4e0ba88bdde48e
Gideon Julian <gideonchinonso77@gmail.com>
2025-07-29 22:32:53 +0100

    updated

--- src/components/FeaturedEvents.jsx
+++ src/components/FeaturedEvents.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import EventCard from "./ui/EventCard";
-
+import event1 from '../../public/events/event1.png'
+import event2 from '../../public/events/event2.png'
+import event3 from '../../public/events/event3.png'
 const FeaturedEvents = () => {
   const eventData = [
     {
       name: "Sail Away Foam Boat Party Phuket",
       date: "Sat, Aug 23rd, 5PM",
-      img: "/events/event1.png",
+      img: event1,
     },
     {
       name: "Don't Tell Mami - Ladies Night",
       date: "Sat, Aug 23rd, 5PM",
-      img: "/events/event2.png",
+      img: event2,
     },
     {
       name: "Flame Dining Show Phuket Town",
       date: "Sat, Aug 23rd, 5PM",
-      img: "/events/event3.png",
+      img: event3,
     },
   ];
+
+  // Duplicate items for looping effect
+  const loopedEvents = [...eventData, ...eventData];
 
   return (
     <div className="w-full p-4 py-10">
@@ -37,14 +42,20 @@ const FeaturedEvents = () => {
         </div>
 
         {/* Carousel */}
-        <div className="mt-10">
+        <div className="mt-10 relative">
           <div
-            className="flex md:flex-wrap gap-6 overflow-x-auto md:overflow-visible scroll-smooth snap-x snap-mandatory scrollbar-hide"
+            className={`
+              flex gap-6 
+              overflow-x-auto md:overflow-visible 
+              scroll-smooth snap-x snap-mandatory 
+              scrollbar-hide md:flex-nowrap 
+              animate-scroll-slow md:animate-none
+            `}
           >
-            {eventData.map((item, idx) => (
+            {loopedEvents.map((item, idx) => (
               <div
                 key={idx}
-                className="flex-shrink-0 w-[80%] sm:w-auto snap-start"
+                className="flex-shrink-0 w-[80%] sm:w-[60%] md:w-[calc(33.333%-16px)] snap-start"
               >
                 <EventCard
                   name={item.name}
@@ -56,7 +67,7 @@ const FeaturedEvents = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div>  
   );
 };
 
