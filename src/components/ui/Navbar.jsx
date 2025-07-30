72e9fc7334c3c12f6ef40858a35227d461cb5a45
Gideon Julian <gideonchinonso77@gmail.com>
2025-07-30 18:49:18 +0100

    updated

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -65,60 +65,36 @@ const Navbar = () => {
       )}
 
       {/* Mobile Menu (Drop from top, center-aligned, 50% width) */}
-    <div
-  className={`fixed left-1/2 top-[20vh] z-50 w-[95%] sm:w-[79%] md:w-[55%] max-w-[760px] -translate-x-1/2 bg-black rounded-2xl text-white p-6 py-7 transition-transform duration-500 ease-in-out ${
-    menuOpen ? "translate-y-0 opacity-100 -mt-10" : "-translate-y-20 opacity-0"
+   <div
+  className={`fixed left-1/2 top-[20vh] z-50 w-[95%] sm:w-[79%] md:w-[55%] max-w-[760px] -translate-x-1/2 bg-black rounded-2xl text-white overflow-hidden origin-top transition-all duration-500 ease-in-out ${
+    menuOpen ? "h-[400px] opacity-100 -mt-10" : "h-0 opacity-0"
   }`}
 >
+  {/* Inner Content fades in */}
+  <div className={`transition-opacity duration-300 ${menuOpen ? "opacity-100" : "opacity-0"} p-6 py-7`}>
+    <div className="mt-8 relative">
+      <Search className="absolute top-3 text-2xl left-2" size={24} />
+      <input
+        placeholder=" Find events"
+        className="w-full px-3 py-3 pl-10 rounded-full border border-white text-white placeholder:text-[19px] placeholder:text-white"
+      />
+    </div>
+    <ul className="flex flex-col gap-4 text-lg pt-4 pb-7">
+      {["DISCOVER EVENTS", "ABOUT US", "CONTACT US", "SHOP"].map((text) => (
+        <li key={text}>
+          <Link
+            to="#"
+            className="font-bold text-2xl"
+            onClick={() => setMenuOpen(false)}
+          >
+            {text}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+</div>
 
-
-        <div className="flex justify-end"></div>
-        <div className="mt-8 relative">
-          <Search className="absolute top-3 text-2xl left-2 " size={24} />
-          <input
-            placeholder=" Find events"
-            className="w-full px-3 py-3 pl-10 rounded-full border border-white text-white placeholder:text-[19px] placeholder:text-white "
-          />
-        </div>
-        <ul className="flex flex-col gap-4 text-lg pt-4 pb-7">
-          <li>
-            <Link
-              className="font-bold text-2xl"
-              to="#"
-              onClick={() => setMenuOpen(false)}
-            >
-              DISCOVER EVENTS
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="font-bold text-2xl"
-              to="#"
-              onClick={() => setMenuOpen(false)}
-            >
-              ABOUT US
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="font-bold text-2xl"
-              to="#"
-              onClick={() => setMenuOpen(false)}
-            >
-              CONTACT US
-            </Link>
-          </li>{" "}
-          <li>
-            <Link
-              className="font-bold text-2xl"
-              to="#"
-              onClick={() => setMenuOpen(false)}
-            >
-              SHOP
-            </Link>
-          </li>
-        </ul>
-      </div>
     </>
   );
 };
