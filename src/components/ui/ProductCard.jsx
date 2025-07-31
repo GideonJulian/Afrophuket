f8293ec46ad6bf46d8af764767c76c5f945418ac
Gideon Julian <gideonchinonso77@gmail.com>
2025-07-31 21:52:23 +0100

    updated

--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -3,10 +3,16 @@ import React from "react";
 
 const ProductCard = ({ imgSrc, name, price }) => {
   return (
-    <div className="w-full sm:w-[400px]">
-      <div className="w-full relative">
-        <img src={imgSrc} className="rounded-2xl w-full h-auto object-cover" />
-        <button className="px-6 py-2 md:py-3 bg-[#000000] rounded-full flex items-center gap-3 text-white absolute bottom-2 right-2">
+    <div className="w-full sm:w-[400px] cursor-pointer transition-transform duration-300 group hover:-translate-y-1">
+      <div className="w-full relative overflow-hidden rounded-2xl">
+        <img
+          src={imgSrc}
+          className="rounded-2xl w-full h-auto object-cover transition-transform duration-300 group-hover:scale-105"
+        />
+        <button
+          className="px-6 py-2 md:py-3 cursor-pointer bg-black rounded-full flex items-center gap-3 text-white 
+          absolute bottom-[-100%] right-2 opacity-0 transition-all duration-300 group-hover:bottom-2 group-hover:opacity-100"
+        >
           <ShoppingCart color="white" size={20} /> Add to cart
         </button>
       </div>
