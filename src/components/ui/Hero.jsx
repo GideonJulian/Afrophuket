bd211c07ae09fc941b6c8130cb544c4cc744c222
Gideon Julian <gideonchinonso77@gmail.com>
2025-08-04 18:02:49 +0100

    updated

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -50,8 +50,8 @@ const Hero = () => {
           <div className="relative inline-block mt-10">
             <span className="absolute inset-0 bg-black rounded-lg translate-x-2 translate-y-2 border-2 "></span>
             <button
-              onClick={() => navigate()}
-              className="relative text-sm font-semibold uppercase px-6 py-3 bg-white text-black rounded-lg border-2 border-black shadow-md scale-105 hover:scale-110 transition-all duration-300"
+              onClick={() => navigate('/event')}
+              className="relative text-sm font-semibold cursor-pointer uppercase px-6 py-3 bg-white text-black rounded-lg border-2 border-black shadow-md scale-105 hover:scale-110 transition-all duration-300"
             >
               Discover Events
             </button>
