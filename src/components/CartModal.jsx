6a3eefec53733c52745461245515b730318674a8
Gideon Julian <gideonchinonso77@gmail.com>
2025-08-05 19:49:23 +0100

    updated

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -21,7 +21,7 @@ const CartModal = ({ isOpen, onClose }) => {
     <AnimatePresence>
       {isOpen && (
         <motion.div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 p-4"
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4"
           onClick={onClose}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
