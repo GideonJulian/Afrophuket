4e6579ef0261d32c85b7c99401a58c1062e9119c
Gideon Julian <gideonchinonso77@gmail.com>
2025-08-07 20:57:09 +0100

    WIP: local changes before pulling

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -45,11 +45,11 @@ const ContactUs = () => {
               className="w-full bg-transparent border-b border-gray-600 py-3 outline-none placeholder:text-gray-500 resize-none"
               rows="4"
             ></textarea>
-            <div className="relative inline-block mt-10">
+            <div className="relative inline-block mt-10 w-full">
               <span className="absolute inset-0 bg-black rounded-lg translate-x-2 translate-y-2 border-2 "></span>
               <button
                 onClick={() => navigate()}
-                className="relative text-sm font-semibold uppercase cursor-pointer px-6 py-3 bg-white text-black rounded-lg border-2 border-black shadow-md scale-105 hover:scale-110 transition-all duration-300"
+                className="relative text-sm w-full font-semibold uppercase cursor-pointer px-6 py-3 bg-white text-black rounded-lg border-2 border-black shadow-md scale-102 hover:scale-105 transition-all duration-300"
               >
                 Submit
               </button>
