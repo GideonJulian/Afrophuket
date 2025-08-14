9f6f166719312b289df6c523ca4c1f5077c4b1a9
Gideon Julian <gideonchinonso77@gmail.com>
2025-08-14 13:02:59 +0100

    updated

--- src/components/Dashboard/Header.jsx
+++ src/components/Dashboard/Header.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-      <div className="flex justify-between items-center pt-20 pl-5">
-        <h1 className="font-bold tex-[25px] ">Events</h1>
+      <div className="flex justify-between items-center pt-20 px-5">
+        <h1 className="font-bold text-[23px] ">Events</h1>
+        <div className="relative inline-block ">
+          <span className="absolute inset-0 bg-black rounded-lg translate-x-2 translate-y-2 border-2 "></span>
+          <button
+            onClick={() => navigate()}
+            className="relative text-sm font-semibold uppercase cursor-pointer px-6 py-3 bg-white text-black rounded-lg border-2 border-black shadow-md scale-105 hover:scale-103 transition-all duration-300"
+          >
+            CREATE NEW EVENT
+          </button>
+        </div>
       </div>
+        <div className="flex justify-between items-center  px-5">
+          <h1 className="">All Events</h1>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
