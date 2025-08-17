684bb3581e9078c0846aa688b4bdbfc4c712d255
Gideon Julian <gideonchinonso77@gmail.com>
2025-08-17 23:43:46 +0100

    updated

--- src/pages/SingleTicket.jsx
+++ src/pages/SingleTicket.jsx
@@ -58,10 +58,10 @@ const SingleTicket = () => {
             />
 
             <div className="relative inline-block mt-10 w-full">
-              <span className="absolute inset-0 bg-black rounded-lg translate-x-3 translate-y-2 border-2 "></span>
+              <span className="absolute inset-0 bg-black rounded-lg translate-x-2 translate-y-2 border-2 "></span>
               <button
                 // onClick={() => navigate()}
-                className="relative text-sm font-semibold uppercase cursor-pointer px-6 py-3 bg-white text-black rounded-lg w-full  border-2 border-black shadow-md scale-105 hover:scale-102 transition-all duration-300"
+                className="relative text-sm font-semibold uppercase cursor-pointer px-6 py-3 bg-white text-black rounded-lg w-full  border-2 border-black shadow-md scale-100 hover:scale-102 transition-all duration-300"
               >
                 GET A TICKET
               </button>
